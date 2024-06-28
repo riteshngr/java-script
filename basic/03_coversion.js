248aa325c68c8e6c1024e6eef47a62a23d02f865
riteshngr <riteshngr12@gmail.com>
2024-06-28 17:32:43 +0000

    coversionn

---
+++ basic/03_coversion.js
@@ -0,0 +1,22 @@
+// convet to int
+let score = undefined
+// change to num use Number
+let fscore = Number(score)
+console.log(typeof(fscore));
+console.log(typeof(score));
+/* varchar = NaN(not a number)
+true and false = num
+strng = NaN
+null = num
+*/
+//covert to boolean
+let achar = 5
+let bolachar = Boolean(achar)
+
+console.log(bolachar);
+// "" = false
+// "fsfjnfsk"= true
+//  convert to string
+let kol = 5457758
+let strkol = String(kol)
+console.log(typeof(strkol));
