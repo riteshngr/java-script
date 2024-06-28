4adf6d6da0ce7048ff8ebc65784cec8d38745dc5
riteshngr <riteshngr12@gmail.com>
2024-06-28 16:03:10 +0000

    2

---
+++ basic/01_constvarprint.js
@@ -0,0 +1,7 @@
+const studentid = "98923"
+const studentname = "arayan"
+let stucity = "hydrabad"
+religion = "hinduu"
+let cast;
+console.log(studentid)
+console.table([studentid,studentname,stucity,religion,cast])
