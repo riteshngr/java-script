a8605ff4ab3cf89df880f4a844386866371f4e69
riteshngr <riteshngr12@gmail.com>
2024-06-28 16:14:35 +0000

    datatype

---
+++ basic/02_datatype.js
@@ -0,0 +1,12 @@
+let namee = "ritesh"
+let  age = 20
+console.log(namee)
+console.log(age)
+let gender;
+let cast = null
+console.log(typeof namee)
+console.log(typeof age)
+console.log(typeof gender)
+console.log(typeof cast)
+console.log()
+
